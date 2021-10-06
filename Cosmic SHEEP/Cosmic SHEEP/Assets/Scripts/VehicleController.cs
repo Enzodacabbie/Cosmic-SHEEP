fd2c40247876d7c8a1c7d6eaeacb1b5a286d9f93
Enzodacabbie <lvcabrera@cpp.edu>
2021-10-06 16:06:10 -0700

    adding movement to the player

--- Cosmic SHEEP/Cosmic SHEEP/Assets/Scripts/VehicleController.cs
+++ Cosmic SHEEP/Cosmic SHEEP/Assets/Scripts/VehicleController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class VehicleController : MonoBehaviour
 {
-    public float givenAmount; 
+    public float givenAmount;
+    public float health;
+    public float maxHealth;
+    public float nextDash;
+    public float dashCooldown;
+    public float dashDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextDash = 0;
+        dashCooldown = 3;
+        dashDistance = 5;
     }
 
     // Update is called once per frame
@@ -16,17 +24,35 @@ public class VehicleController : MonoBehaviour
     {
         float dy = Input.GetAxis("Vertical");
         float dx = Input.GetAxis("Horizontal");
+        float dashx = Input.GetAxis("Dashing");
 
         if(Mathf.Abs(dy)> 0.001f) //if we are moving vertically
         {
-           transform.localPosition += new Vector3(0, dy*givenAmount*Time.deltaTime, 0);
+            transform.localPosition += new Vector3(0, dy*givenAmount*Time.deltaTime, 0);
             StayInBoundsY();
         }
         if(Mathf.Abs(dx) > 0.001f) //if we are moving horizontally
         {
-           transform.localPosition += new Vector3(dx*givenAmount*Time.deltaTime, 0, 0);
+            transform.localPosition += new Vector3(dx*givenAmount*Time.deltaTime, 0, 0);
             StayInBoundsX();
         }
+        if(Mathf.Abs(dashx) > 0.001f && Time.time > nextDash) //if there is dash input and cooldown is done
+        {
+            Debug.Log("Dashing");
+            if(dashx > 0.0f) //dash to the right
+            {
+                print(dashx);
+                transform.localPosition += new Vector3(dashDistance, 0, 0);
+            }
+            else //dash to the left
+            {
+                transform.localPosition += new Vector3(-dashDistance, 0, 0);
+            }
+            nextDash = Time.time + dashCooldown; //nextDash becomes greater than Time so cannot be done until time passes it
+            StayInBoundsX();
+            
+        }
+        
     }
     //This method clamps the player position to the ends of the screen
     void StayInBoundsX() 
