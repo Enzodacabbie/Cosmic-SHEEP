3d47c591f1eff574f433ef26855c2546353a209a
Enzodacabbie <lvcabrera@cpp.edu>
2021-10-25 15:22:47 -0700

    fixed player rotation, however the player can only move forward in the z direction

--- Cosmic SHEEP/Cosmic SHEEP/Assets/Scripts/VehicleController.cs
+++ Cosmic SHEEP/Cosmic SHEEP/Assets/Scripts/VehicleController.cs
@@ -24,6 +24,7 @@ public class VehicleController : MonoBehaviour
     public CinemachineDollyCart cart;
     public Camera cam;
     public GameObject aimTarget;
+    public GameObject thing;
 
 
     // Start is called before the first frame update
@@ -31,7 +32,7 @@ public class VehicleController : MonoBehaviour
     {
         cart = this.GetComponentInParent<CinemachineDollyCart>();
 
-        speed = 20;
+        speed = 10;
         nextDash = 0f;
         dashCooldown = 3f;
         dashDistance = 5f;
@@ -58,14 +59,14 @@ public class VehicleController : MonoBehaviour
         float movement = Input.GetAxis("Boost");
 
         if(Mathf.Abs(dy)> 0.001f && moveable == true) //if we are moving vertically
-        {
-            lookAtTarget(aimTarget.transform.position, dx, dy, lookSpeed);
+        { 
             move(dx, dy, speed);
+           
         }
         if (Mathf.Abs(dx) > 0.001f && moveable == true) //if we are moving horizontally
         {
-            lookAtTarget(aimTarget.transform.position, dx, dy, lookSpeed);
             move(dx, dy, speed);
+            Lean(thing, dx, 80, 0.1f);
         }
 
         if(Mathf.Abs(dashx) > 0.001f && Time.time > nextDash) //if there is dash input and cooldown is done
@@ -94,21 +95,16 @@ public class VehicleController : MonoBehaviour
 
     void move(float x, float y, float speed)
     {
+        Vector3 look = new Vector3(x, y, 1.0f); ;
         transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(look), Mathf.Deg2Rad * 50.0f);
         StayInBounds();
     }
 
-    void lookAtTarget(Vector3 pos, float x, float y, float lookspeed)
+    void Lean(GameObject x, float dx, float leanLimit, float time)
     {
-        Quaternion look = Quaternion.LookRotation(aimTarget.transform.position);
-        aimTarget.transform.localPosition = new Vector3(x*2, (y+1)*2, 6);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, look, Mathf.Deg2Rad * lookSpeed * Time.deltaTime);
-        transform.LookAt(aimTarget.transform);
-    }
-
-    void Lean()
-    {
-
+        Vector3 target = transform.localEulerAngles;
+        x.transform.localEulerAngles = new Vector3(target.x, target.y, Mathf.LerpAngle(target.z, -dx * leanLimit, time));
     }
 
     void changeSpeed(float boostSpeed)
@@ -195,20 +191,21 @@ public class VehicleController : MonoBehaviour
     {
         hittable = false;
         canBoost = false;
-        Vector3 endPosition = new Vector3(transform.localPosition.x+5f, transform.position.y, transform.position.z);
-        float savedHealth = health; 
+        moveable = false;
+        float savedHealth = health;
         float i = 0.0f;
-
+        Vector3 endPosition = new Vector3(transform.localPosition.x+5f, transform.position.y, transform.position.z);
+        
         health = 1000.0f;
         while (transform.position != endPosition &&  i < time)
         {
             i += Time.deltaTime;
-            //this.transform.position = Vector3.MoveTowards(this.transform.position, endPosition, 20f * Time.deltaTime);
             transform.Rotate(0f, 0f, 360f * time * Time.deltaTime, Space.Self);
-           
             yield return null;
         }
+
         health = savedHealth;
+        moveable = true;
         hittable = true;
         canBoost = true;
     }
@@ -217,20 +214,21 @@ public class VehicleController : MonoBehaviour
     {
         hittable = false;
         canBoost = false;
-        Vector3 endPosition = new Vector3(transform.localPosition.x - 5f, transform.position.y, transform.position.z);
+        moveable = false;
         float savedHealth = health;
         float i = 0.0f;
+        Vector3 endPosition = new Vector3(transform.localPosition.x - 5f, transform.position.y, transform.position.z);
 
         health = 1000.0f;
         while (transform.position != endPosition && i < time)
         {
             i += Time.deltaTime;
-            //this.transform.position = Vector3.MoveTowards(this.transform.position, endPosition, 20f * Time.deltaTime);
             transform.Rotate(0f, 0f, 360f * time * Time.deltaTime, Space.Self);
-
             yield return null;
         }
+
         health = savedHealth;
+        moveable = true;
         hittable = true;
         canBoost = true;
     }
